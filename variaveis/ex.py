1d2c4f803615095056f954c036e7027f4147c81d
samyasouzasoares8312-debug <samyasouzasoares8312@gmail.com>
2025-09-04 21:02:34 -0300

    oi

---
+++ variaveis/ex.py
@@ -0,0 +1,14 @@
+# idade = 20
+# if idade == 18:
+  #  print("Você pode fazer parte da festa")
+# print("passe livre")
+idade = int(input("Digite a tua idade: "))
+# condicional simples a composta usando if else e else 
+
+if idade < 12: 
+    print("Você é criança e não pode assistir TV até tarde")
+elif idade <= 18:
+    print("You're teenager")
+else:
+    print("Você é adulto")
+
